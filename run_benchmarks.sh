59abeabfa975de19ab5a061a62ff5c75f78b0346
Andrej Pištek <andrej.pistek@outlook.com>
2023-04-13 13:02:30 +0200

    DPMC and safety synthesis now both compile and run with new sylvan

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -4,24 +4,25 @@ run_dpmc_benchmark()
     cnf_file_path=$1
     echo "Running DPMC benchmark"
     cd ./build/dpmc || exit 
-   ./lg "./htd_main-1.2.0 -s 1234567 --print-progress --strategy challenge --opt width --iterations 0 --preprocessing full --patience 20" < $cnf_file_path | ./dmc --pw=3 --cf $cnf_file_path --dp=s --lc=1 --wc=0 --vs=2 --dv=5 --dy=0 --mm=6500 --jp=f --sa=0 --aa=1 --er=0 --tc=7 --tr=3 --ir=5
+   # shellcheck disable=SC2094
+   ./lg "./htd_main-1.2.0 -s 1234567 --print-progress --strategy challenge --opt width --iterations 0 --preprocessing full --patience 20" < "$cnf_file_path" | ./dmc --pw=3 --cf "$cnf_file_path" --dp=s --lc=1 --wc=0 --vs=2 --dv=5 --dy=0 --mm=6500 --jp=f --sa=0 --aa=1 --er=0 --tc=7 --tr=3 --ir=5
     cd ../../ || exit 
 }
 
 # precondition: the build directory exists and contains the executable aiger_synt + the input data
 run_safety_synthesis_benchmark()
 {
-    input_file_path=$1
+    agg_file_path=$1
     echo "Running safety synthesis benchmark"
     cd ./build/safety_synthesis || exit 
-    ./aiger_synt ./add10n.aag -w 4
+    ./aiger_synt "$agg_file_path" -w 4
     cd ../../ || exit 
 }
 
 # precondition: the build directory exists and contains the executables storm + the input data
 run_storm_benchmark()
 {
-    input_file_path=$1
+#    input_file_path=$1
     echo "Running STORM benchmark"
     cd ./build/storm || exit 
     # run benchmark
@@ -29,5 +30,5 @@ run_storm_benchmark()
 }
 
 run_dpmc_benchmark "./mcc21__wff.3.75.315.cnf"
-run_safety_synthesis_benchmark 
+run_safety_synthesis_benchmark "./add10n.aag"
 # run_storm_benchmark 
