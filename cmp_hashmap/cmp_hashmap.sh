2eac66d9c50fe38d8fc71ded6d6d8bae0308acbe
Andrej Pištek <andrej.pistek@outlook.com>
2023-04-17 18:38:58 +0200

    fix benchmark typo and update sylvan

--- cmp_hashmap/cmp_hashmap.sh
+++ cmp_hashmap/cmp_hashmap.sh
@@ -18,12 +18,12 @@ rm -f build
 mkdir build
 cd build || exit
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DDSYLVAN_USE_LINEAR_PROBING=ON -DSYLVAN_LIMIT_PROBE_SEQUENCE=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DSYLVAN_USE_LINEAR_PROBING=ON -DSYLVAN_LIMIT_PROBE_SEQUENCE=ON
 compile_cmp_hashmap
 echo "Running cmp_hashmap with linear probing + limit the probe sequence"
 run_cmp_hashmap
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DDSYLVAN_USE_LINEAR_PROBING=OFF -DSYLVAN_LIMIT_PROBE_SEQUENCE=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DSYLVAN_USE_LINEAR_PROBING=OFF -DSYLVAN_LIMIT_PROBE_SEQUENCE=OFF
 compile_cmp_hashmap
 echo "Running cmp_hashmap with linear probing + dont limit the probe sequence"
 run_cmp_hashmap
