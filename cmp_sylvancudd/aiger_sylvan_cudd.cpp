59f9b324dc55a3241c100193b6882b910866d5c7
Andrej Pištek <andrej.pistek@outlook.com>
2023-07-05 13:16:20 +0200

    updated sylvan vs cudd aiger

--- cmp_sylvancudd/aiger_sylvan_cudd.cpp
+++ cmp_sylvancudd/aiger_sylvan_cudd.cpp
@@ -1,9 +1,6 @@
 #include <getopt.h>
 
 #include <sys/time.h>
-#include <boost/graph/sloan_ordering.hpp>
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
 #include <string>
 #include <fcntl.h>
 #include <span>
@@ -15,14 +12,19 @@
 
 using namespace sylvan;
 
-typedef struct safety_game
+typedef struct cudd_safety_game
 {
-    MTBDD  *s_gates;            // Sylvan and gates
-    DdNode **c_gates;            // CUDD and gates
-    MTBDD   c_inputs;           // controllable inputs
-    MTBDD   u_inputs;           // uncontrollable inputs
-    int    *level_to_order;     // mapping from variable level to static variable order
-} safety_game_t;
+    DdNode      **gates;                // and gates
+    DdNode      **c_inputs;             // controllable inputs
+    DdNode      **u_inputs;             // uncontrollable inputs
+} cudd_safety_game_t;
+
+typedef struct sylvan_safety_game
+{
+    MTBDD  *gates;                      // Sylvan and gates
+    MTBDD   c_inputs;                   // controllable inputs
+    MTBDD   u_inputs;                   // uncontrollable inputs
+} sylvan_safety_game_t;
 
 double t_start;
 #define INFO(s, ...) fprintf(stdout, "\r[% 8.2f] " s, wctime()-t_start, ##__VA_ARGS__)
@@ -33,10 +35,7 @@ double t_start;
 static int workers = 1;
 static int verbose = 0;
 static char *filename = nullptr; // filename of the aag file
-static int static_reorder = 0;
 static int dynamic_reorder = 0;
-static int sloan_w1 = 1;
-static int sloan_w2 = 8;
 
 //static FILE *log_file = nullptr;
 
@@ -63,17 +62,23 @@ static aag_file_t aag{
         .gatergt = nullptr
 };
 static aag_buffer_t aag_buffer{
-        .content = NULL,
+        .content = nullptr,
         .size = 0,
         .pos = 0,
         .file_descriptor = -1,
         .filestat = {}
 };
-static safety_game_t game{
-        .s_gates = nullptr,
+
+static sylvan_safety_game_t sylvan_game{
+        .gates = nullptr,
         .c_inputs = sylvan_set_empty(),
-        .u_inputs = sylvan_set_empty(),
-        .level_to_order = nullptr
+        .u_inputs = sylvan_set_empty()
+};
+
+static cudd_safety_game_t cudd_game{
+        .gates = nullptr,
+        .c_inputs = nullptr,
+        .u_inputs = nullptr
 };
 
 /* Obtain current wallclock time */
@@ -125,7 +130,6 @@ parse_args(int argc, char **argv)
                 workers = atoi(optarg);
                 break;
             case 's':
-                static_reorder = 1;
                 break;
             case 'd':
                 dynamic_reorder = 1;
@@ -171,7 +175,7 @@ VOID_TASK_0(reordering_end)
 #define make_gate(gate, db) CALL(make_gate, gate, db)
 VOID_TASK_2(make_gate, int, gate, DdManager*, db)
 {
-    if (game.s_gates[gate] != sylvan_invalid) return;
+    if (sylvan_game.gates[gate] != sylvan_invalid) return;
     int lft = (int) aag.gatelft[gate] / 2;
     int rgt = (int) aag.gatergt[gate] / 2;
 
@@ -183,22 +187,22 @@ VOID_TASK_2(make_gate, int, gate, DdManager*, db)
         c_l = CUDD_FALSE;
     } else if (aag.lookup[lft] != -1) {
         make_gate(aag.lookup[lft], db);
-        l = game.s_gates[aag.lookup[lft]];
-        c_l = game.c_gates[aag.lookup[lft]];
+        l = sylvan_game.gates[aag.lookup[lft]];
+        c_l = cudd_game.gates[aag.lookup[lft]];
     } else {
-        l = sylvan_ithvar(game.level_to_order[lft]); // always use even variables (prime is odd)
-        c_l = Cudd_bddIthVar(db, game.level_to_order[lft]);
+        l = sylvan_ithvar(lft); // always use even variables (prime is odd)
+        c_l = Cudd_bddIthVar(db, lft);
     }
     if (rgt == 0) {
         r = sylvan_false;
         c_r = CUDD_FALSE;
     } else if (aag.lookup[rgt] != -1) {
         make_gate(aag.lookup[rgt], db);
-        r = game.s_gates[aag.lookup[rgt]];
-        c_r = game.c_gates[aag.lookup[rgt]];
+        r = sylvan_game.gates[aag.lookup[rgt]];
+        c_r = cudd_game.gates[aag.lookup[rgt]];
     } else {
-        r = sylvan_ithvar(game.level_to_order[rgt]); // always use even variables (prime is odd)
-        c_r = Cudd_bddIthVar(db, game.level_to_order[rgt]);
+        r = sylvan_ithvar(rgt); // always use even variables (prime is odd)
+        c_r = Cudd_bddIthVar(db, rgt);
     }
     if (aag.gatelft[gate] & 1) {
         l = sylvan_not(l);
@@ -209,47 +213,50 @@ VOID_TASK_2(make_gate, int, gate, DdManager*, db)
         c_r = Cudd_Not(c_r);
     }
 
-    game.s_gates[gate] = sylvan_and(l, r);
-    mtbdd_protect(&game.s_gates[gate]);
+    sylvan_game.gates[gate] = sylvan_and(l, r);
+    mtbdd_protect(&sylvan_game.gates[gate]);
 
-    game.c_gates[gate] = Cudd_bddAnd(db, c_l, c_r);
-    Cudd_Ref(game.c_gates[gate]);
+    cudd_game.gates[gate] = Cudd_bddAnd(db, c_l, c_r);
+    Cudd_Ref(cudd_game.gates[gate]);
 }
 
 #define solve_game(db) RUN(solve_game, db)
 TASK_1(int, solve_game, DdManager*, db)
 {
-    game.level_to_order = new int[aag.header.m + 1];
-    for (int i = 0; i <= (int) aag.header.m; i++) game.level_to_order[i] = i;
-
-    game.s_gates = new MTBDD[aag.header.a];
-    for (uint64_t a = 0; a < aag.header.a; a++) game.s_gates[a] = sylvan_invalid;
-
-    game.c_gates = new DdNode*[aag.header.a];
-    for (uint64_t a = 0; a < aag.header.a; a++) game.c_gates[a] = nullptr;
-
+    sylvan_game.gates = new MTBDD[aag.header.a];
+    for (uint64_t a = 0; a < aag.header.a; a++) sylvan_game.gates[a] = sylvan_invalid;
+    cudd_game.gates = new DdNode*[aag.header.a];
+    for (uint64_t a = 0; a < aag.header.a; a++) cudd_game.gates[a] = nullptr;
 
     INFO("Making the gate BDDs...\n");
 
     for (uint64_t gate = 0; gate < aag.header.a; gate++) {
         make_gate(gate, db);
+//        sylvan_test_reduce_heap();
+//        INFO("Sylvan: %5zu Cudd: %zu\n", llmsset_count_marked(nodes), Cudd_ReadNodeCount(db));
+        if (llmsset_count_marked(nodes) >= reorder_db->config.size_threshold) {
+            sylvan_reduce_heap(SYLVAN_REORDER_BOUNDED_SIFT);
+            Cudd_ReduceHeap(db, CUDD_REORDER_SIFT, 0);
+        }
     }
 
-    INFO("Sylvan gates have size %zu\n", mtbdd_nodecount_more(game.s_gates, aag.header.a));
-    INFO("Cudd   gates have size %zu\n", Cudd_ReadNodeCount(db));
+//    printf("Sylvan levels count:  %zu\n", levels_get_count(&reorder_db->levels));
+//    printf("Cudd levels count:    %d\n", Cudd_ReadSize(db));
+//
+//    INFO("Sylvan gates have size %zu\n", mtbdd_nodecount_more(sylvan_game.gates, aag.header.a));
+//    INFO("Cudd   gates have size %zu\n", Cudd_ReadNodeCount(db));
+//
+//    sylvan_reduce_heap(SYLVAN_REORDER_BOUNDED_SIFT);
+//    Cudd_ReduceHeap(db, CUDD_REORDER_SIFT, 0);
+//
+//    INFO("Sylvan gates have size %zu\n", mtbdd_nodecount_more(sylvan_game.gates, aag.header.a));
+//    INFO("Cudd   gates have size %zu\n", Cudd_ReadNodeCount(db));
 
-    sylvan_reduce_heap(SYLVAN_REORDER_BOUNDED_SIFT);
-    Cudd_ReduceHeap(db, CUDD_REORDER_SIFT, 0);
+    sylvan_game.c_inputs = sylvan_set_empty();
+    sylvan_game.u_inputs = sylvan_set_empty();
+    mtbdd_protect(&sylvan_game.c_inputs);
+    mtbdd_protect(&sylvan_game.u_inputs);
 
-    INFO("Sylvan gates have size %zu\n", mtbdd_nodecount_more(game.s_gates, aag.header.a));
-    INFO("Cudd   gates have size %zu\n", Cudd_ReadNodeCount(db));
-
-    exit(1);
-
-    game.c_inputs = sylvan_set_empty();
-    game.u_inputs = sylvan_set_empty();
-    mtbdd_protect(&game.c_inputs);
-    mtbdd_protect(&game.u_inputs);
 
     // Now read the [[optional]] labels to find controllable vars
     while (true) {
@@ -263,57 +270,13 @@ TASK_1(int, solve_game, DdManager*, db)
         aag_buffer_read_wsnl(&aag_buffer);
         if (c == 'i') {
             if (strncmp(s.c_str(), "controllable_", 13) == 0) {
-                game.c_inputs = sylvan_set_add(game.c_inputs, game.level_to_order[aag.inputs[pos] / 2]);
+                sylvan_game.c_inputs = sylvan_set_add(sylvan_game.c_inputs, aag.inputs[pos] / 2);
             } else {
-                game.u_inputs = sylvan_set_add(game.u_inputs, game.level_to_order[aag.inputs[pos] / 2]);
+                sylvan_game.u_inputs = sylvan_set_add(sylvan_game.u_inputs, aag.inputs[pos] / 2);
             }
         }
     }
-    INFO("There are %zu controllable and %zu uncontrollable inputs.\n", sylvan_set_count(game.c_inputs), sylvan_set_count(game.u_inputs));
-
-#if 0
-    sylvan_print(Xc);
-    printf("\n");
-    sylvan_print(Xu);
-    printf("\n");
-    for (uint64_t g=0; g<A; g++) {
-        INFO("gate %d has size %zu\n", (int)g, sylvan_nodecount(gates[g]));
-    }
-
-    for (uint64_t g=0; g<A; g++) {
-        MTBDD supp = sylvan_support(gates[g]);
-        while (supp != sylvan_set_empty()) {
-            printf("%d ", sylvan_set_first(supp));
-            supp = sylvan_set_next(supp);
-        }
-        printf("\n");
-    }
-
-    MTBDD lnext[L];
-    for (uint64_t l=0; l<L; l++) {
-        MTBDD nxt;
-        if (lookup[l_next[l]/2] == -1) {
-            nxt = sylvan_ithlevel(l_next[l]&1);
-        } else {
-            nxt = gates[lookup[l_next[l]]];
-        }
-        if (l_next[l]&1) nxt = sylvan_not(nxt);
-        lnext[l] = sylvan_equiv(sylvan_ithvar(latches[l]+1), nxt);
-    }
-    INFO("done making latches\n");
-
-    MTBDD Lvars = sylvan_set_empty();
-    mtbdd_protect(&Lvars);
-
-    for (uint64_t l = 0; l < aag.header.l; l++) {
-        Lvars = sylvan_set_add(Lvars, game.level_to_order[aag.latches[l] / 2]);
-    }
-
-    MTBDD LtoPrime = sylvan_map_empty();
-    for (uint64_t l=0; l<L; l++) {
-        LtoPrime = sylvan_map_add(LtoPrime, latches[l], latches[l]+1);
-    }
-#endif
+    INFO("There are %zu controllable and %zu uncontrollable inputs.\n", sylvan_set_count(sylvan_game.c_inputs), sylvan_set_count(sylvan_game.u_inputs));
 
     // Actually just make the compose vector
     MTBDD CV = sylvan_map_empty();
@@ -322,12 +285,12 @@ TASK_1(int, solve_game, DdManager*, db)
     for (uint64_t l = 0; l < aag.header.l; l++) {
         MTBDD nxt;
         if (aag.lookup[aag.l_next[l] / 2] == -1) {
-            nxt = sylvan_ithvar(game.level_to_order[aag.l_next[l] / 2]);
+            nxt = sylvan_ithvar(aag.l_next[l] / 2);
         } else {
-            nxt = game.s_gates[aag.lookup[aag.l_next[l] / 2]];
+            nxt = sylvan_game.gates[aag.lookup[aag.l_next[l] / 2]];
         }
         if (aag.l_next[l] & 1) nxt = sylvan_not(nxt);
-        CV = sylvan_map_add(CV, game.level_to_order[aag.latches[l] / 2], nxt);
+        CV = sylvan_map_add(CV, aag.latches[l] / 2, nxt);
     }
 
     // now make output
@@ -336,22 +299,12 @@ TASK_1(int, solve_game, DdManager*, db)
     if (aag.lookup[aag.outputs[0] / 2] == -1) {
         Unsafe = sylvan_ithvar(aag.outputs[0] / 2);
     } else {
-        Unsafe = game.s_gates[aag.lookup[aag.outputs[0] / 2]];
+        Unsafe = sylvan_game.gates[aag.lookup[aag.outputs[0] / 2]];
     }
     if (aag.outputs[0] & 1) Unsafe = sylvan_not(Unsafe);
-    Unsafe = sylvan_forall(Unsafe, game.c_inputs);
-    Unsafe = sylvan_exists(Unsafe, game.u_inputs);
+    Unsafe = sylvan_forall(Unsafe, sylvan_game.c_inputs);
+    Unsafe = sylvan_exists(Unsafe, sylvan_game.u_inputs);
 
-
-#if 0
-    MTBDD supp = sylvan_support(Unsafe);
-    while (supp != sylvan_set_empty()) {
-        printf("%d ", sylvan_set_first(supp));
-        supp = sylvan_set_next(supp);
-    }
-    printf("\n");
-    INFO("exactly %.0f states are bad\n", sylvan_satcount(Unsafe, Lvars));
-#endif
 
     MTBDD OldUnsafe = sylvan_false; // empty set
     MTBDD Step = sylvan_false;
@@ -362,8 +315,8 @@ TASK_1(int, solve_game, DdManager*, db)
         OldUnsafe = Unsafe;
 
         Step = sylvan_compose(Unsafe, CV);
-        Step = sylvan_forall(Step, game.c_inputs);
-        Step = sylvan_exists(Step, game.u_inputs);
+        Step = sylvan_forall(Step, sylvan_game.c_inputs);
+        Step = sylvan_exists(Step, sylvan_game.u_inputs);
 
         // check if initial state in Step (all 0)
         MTBDD Check = Step;
@@ -393,8 +346,7 @@ int main(int argc, char **argv)
     aag_buffer_open(&aag_buffer, filename, O_RDONLY);
     aag_file_read(&aag, &aag_buffer);
 
-//    if (verbose) {
-    if (0) {
+    if (verbose) {
         INFO("----------header----------\n");
         INFO("# of variables            \t %lu\n", aag.header.m);
         INFO("# of inputs               \t %lu\n", aag.header.i);
@@ -423,7 +375,7 @@ int main(int argc, char **argv)
     // Cache table size: 36 bytes * cache entries
     // With 2^20 nodes and 2^18 cache entries, that's 33 MB
     // With 2^24 nodes and 2^22 cache entries, that's 528 MB
-    sylvan_set_sizes(1LL<<14, 1LL<<18, 1LL<<14, 1LL<<18);
+    sylvan_set_sizes(1LL<<14, 1LL<<25, 1LL<<14, 1LL<<19);
     sylvan_init_package();
     sylvan_set_granularity(2); // granularity 3 is decent value for this small problem - 1 means "use cache for every operation"
     sylvan_init_mtbdd();
