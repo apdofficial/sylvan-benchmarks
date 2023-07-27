6f438fee79b591b45b7573618e6b68ef232b5a6c
Andrej Pištek <andrej.pistek@outlook.com>
2023-07-27 13:38:40 +0200

    update names

--- syntcomp/tests/txt_to_csv.py
+++ syntcomp/tests/txt_to_csv.py
@@ -9,44 +9,44 @@ results_auto = results / 'auto'
 
 
 @dataclass
-class Size:
+class Reordering:
     from_size: int
     to_size: int
     time: float
 
 
 @dataclass
-class SizeBenchmark:
+class Benchmark:
     path: Path
-    results: [Size]
+    reorderings: [Reordering]
 
 
-def txt_size_benchmark(path: Path) -> SizeBenchmark:
+def txt_size_benchmark(path: Path) -> Benchmark:
     with open(path, 'r') as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines if line.startswith("BDD")]
-        benchmark = SizeBenchmark(path, [])
+        benchmark = Benchmark(path, [])
         for line in lines:
             line = re.findall(r'\b\d+\b', line)
             if len(line) == 4:
-                benchmark.results.append(Size(int(line[0]), int(line[1]), float(line[2] + "." + line[3])))
+                benchmark.reorderings.append(Reordering(int(line[0]), int(line[1]), float(line[2] + "." + line[3])))
             else:
-                benchmark.results.append(Size(int(line[0]), int(line[1]), int(line[2])))
+                benchmark.reorderings.append(Reordering(int(line[0]), int(line[1]), int(line[2])))
         return benchmark
 
 
-def extract_size_benchmarks(path: Path) -> [SizeBenchmark]:
+def extract_size_benchmarks(path: Path) -> [Benchmark]:
     size_results = list(path.glob('*.txt'))
-    benchmarks: [SizeBenchmark] = []
+    benchmarks: [Benchmark] = []
     for path in size_results:
         benchmarks.append(txt_size_benchmark(path))
     return benchmarks
 
 
-def write_benchmarks_to_csv(benchmarks: [SizeBenchmark]):
+def write_benchmarks_to_csv(benchmarks: [Benchmark]):
     for benchmark in benchmarks:
         with open(benchmark.path.with_suffix('.csv'), 'w') as f:
-            w = DataclassWriter(f, benchmark.results, Size)
+            w = DataclassWriter(f, benchmark.reorderings, Reordering)
             w.write()
 
 
